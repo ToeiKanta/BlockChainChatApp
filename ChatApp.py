a4dd6f9b9b56512074ae2b1ce2c087af98d58b30
ToeiKanta <41673034+ToeiKanta@users.noreply.github.com>
2020-01-19 03:05:25 +0700

    Update Client decode msg

--- ChatApp.py
+++ ChatApp.py
@@ -3,6 +3,7 @@ import threading
 import sys, os
 import random
 import string
+import json
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'BlockChain'))
 import BlockChain
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'AES_Cyptography'))
@@ -20,22 +21,23 @@ class Server:
 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	connections = []
 	def __init__(self):
-		self.sock.bind(('0.0.0.0',20002))
+		self.sock.bind(('0.0.0.0',20003))
 		self.sock.listen(1)
 	def sendBoardcast(self):
 		while True:
 			data = bytes(input(""),'utf-8')
 			for connection in self.connections:
 				connection.send(data)
+
+	# recieve msg handler from Client
 	def handler(self,c,a):
 		while True:
 			data = c.recv(1024)
-			# Mining Block
-			block = BlockChain.Block(data)
+			#create new block
+			block = BlockChain.Block(data.decode('utf-8'))
 			blockchain.mine(block)
 			for connection in self.connections:
-				connection.send(bytes(str(block),'utf-8'))
-				# print(data)
+				connection.send(bytes(str(block.data),'utf-8'))
 			if not data:
 				print(str(a[0])+':'+str(a[1])+" disconnected")
 				self.connections.remove(c)
@@ -58,16 +60,23 @@ class Client:
 	key_aes = ''
 	name = ''
 	groupId = ''
+	padding_character = "X"
 	sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 	def sendMsg(self):
 		while True:
 			private_msg = input("")
 			secret_key = self.key_aes
-			padding_character = "X"
-			data = bytes("{ groupID: " + str(self.groupId)+ ";name: "+ str(self.name) +";msg: " + str(AES_Cyptography.encrypt_message(private_msg, secret_key, padding_character))  + "}",'utf-8')
-			self.sock.send(data)
+			padding_character = self.padding_character
+			data = {
+				"groupId" : str(self.groupId),
+				"sender" : str(self.name),
+				"msg" : str(AES_Cyptography.encrypt_message(private_msg, secret_key, padding_character).decode('utf-8'))
+			}
+			jsonData = json.dumps(data)
+			# data = bytes('{ "groupId": "' + str(self.groupId)+ '","sender": "'+ str(self.name) +'","msg": "' + str(AES_Cyptography.encrypt_message(private_msg, secret_key, padding_character))  + '"}','utf-8')
+			self.sock.send(bytes(jsonData,"utf-8"))
 	def __init__(self,address):
-		self.sock.connect((address,20002))
+		self.sock.connect((address,20003))
 		print("Welcome to BlockChat")
 		print("Enter your name :")
 		self.name = input("")
@@ -92,11 +101,17 @@ class Client:
 		iThread = threading.Thread(target=self.sendMsg)
 		iThread.daemon = True
 		iThread.start()
+		# recieve msg from server
 		while True:
 			data = self.sock.recv(1024)
 			if not data:
 				break
-			print(data)
+			newBlock = json.loads(data.decode("utf-8"))
+			groupId = newBlock["groupId"]
+			sender = newBlock["sender"]
+			msg = newBlock["msg"]
+			if (groupId == self.groupId):
+				print(str(sender) + " : " + str(AES_Cyptography.decrypt_message(msg, self.key_aes, self.padding_character).decode('utf-8')))
 
 if(len(sys.argv)>1):
 	client = Client(sys.argv[1])
