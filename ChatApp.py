173bb4d39576719dc6ae6d9fd4d958b25832d85a
ToeiKanta <41673034+ToeiKanta@users.noreply.github.com>
2020-01-19 04:36:48 +0700

    Update UI Readme and command

--- ChatApp.py
+++ ChatApp.py
@@ -28,11 +28,14 @@ def randomString(stringLength=10):
     letters = string.ascii_lowercase
     return ''.join(random.choice(letters) for i in range(stringLength))
 
+# Server 
 class Server:
+	port = 0
 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	connections = []
-	def __init__(self):
-		self.sock.bind(('0.0.0.0',PORT))
+	def __init__(self,port):
+		self.port = port
+		self.sock.bind(('0.0.0.0',int(port)))
 		self.sock.listen(1)
 	def sendBoardcast(self):
 		while True:
@@ -91,6 +94,7 @@ $$ |  $$\ $$ |  $$ |$$  __$$ | $$ |$$\
                                             
 """)
 		print("//////////// This is SERVER ///////////////////")
+		print("SERVER running on port "+str(socket.gethostbyname(socket.gethostname()))+":"+str(self.port))
 		while True:
 			c,a = self.sock.accept()
 			cThread = threading.Thread(target=self.handler,args=(c,a))
@@ -122,8 +126,8 @@ class Client:
 			}
 			jsonData = json.dumps(data)
 			self.sock.send(bytes(jsonData,"utf-8"))
-	def __init__(self,address):
-		self.sock.connect((address,PORT))
+	def __init__(self,address,port):
+		self.sock.connect((address,int(port)))
 		print(Fore.LIGHTGREEN_EX)
 		os.system('cls')
 		os.system('clear')
@@ -166,33 +170,36 @@ $$ |  $$\ $$ |  $$ |$$  __$$ | $$ |$$\
 		print("ENTER 1 to CREATE new group:")
 		print("ENTER 2 to JOIN group:")
 		print("==========================")
-		mode = input("")
-		# create new group
-		if(mode == "1"):
-			print("/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/")
-			print("You select mode 1 (create new group chat):")
-			print("/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/")
-			# create AES key
-			self.key_aes = AES_Cyptography.generate_secret_key_for_AES_cipher()
-			# create group ID
-			self.groupId = randomString(10).upper()
-			print("==============================================================")
-			print(Fore.LIGHTGREEN_EX,"This is your group id (send to your friend) : ")
-			print(Fore.LIGHTCYAN_EX,str(self.groupId))
-			print(Fore.LIGHTGREEN_EX,"This is yout secrete group key (send to your friend) : ")
-			print(Fore.LIGHTCYAN_EX,str(self.key_aes.decode("utf-8")))
-			print(Fore.LIGHTGREEN_EX,"==============================================================")
-		elif(mode == "2"):
-			print("==================================")
-			print("Enter Group ID :")
-			self.groupId = input("")
-			print("Please enter secrete group key :")
-			self.key_aes = input("")
-		print(Fore.LIGHTRED_EX)
-		print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+		while True:
+			mode = input("")
+			# create new group
+			if(mode == "1"):
+				print("/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/")
+				print("You select mode 1 (create new group chat):")
+				print("/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/=/")
+				# create AES key
+				self.key_aes = AES_Cyptography.generate_secret_key_for_AES_cipher()
+				# create group ID
+				self.groupId = randomString(10).upper()
+				print("==============================================================")
+				print(Fore.LIGHTGREEN_EX,"This is your group id (send to your friend) : ")
+				print(Fore.LIGHTCYAN_EX,str(self.groupId))
+				print(Fore.LIGHTGREEN_EX,"This is yout secrete group key (send to your friend) : ")
+				print(Fore.LIGHTCYAN_EX,str(self.key_aes.decode("utf-8")))
+				print(Fore.LIGHTGREEN_EX,"==============================================================")
+				break
+			elif(mode == "2"):
+				print("==================================")
+				print("Enter Group ID :")
+				self.groupId = input("")
+				print("Please enter secrete group key :")
+				self.key_aes = input("")
+				break
+		print(Fore.LIGHTYELLOW_EX)
+		print("////////// WELCOME TO CHAT ROOM ///////////")
 		print(" CHAT ROOM Group_id : "+self.groupId +" Started!!!")
 		print(" You are : "+self.name)
-		print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+		print("///////////////////////////////////////////")
 		print(Fore.LIGHTGREEN_EX)
 		iThread = threading.Thread(target=self.sendMsg)
 		iThread.daemon = True
@@ -209,8 +216,8 @@ $$ |  $$\ $$ |  $$ |$$  __$$ | $$ |$$\
 			if (groupId == self.groupId):
 				print(str(sender) + " : " + str(AES_Cyptography.decrypt_message(msg, self.key_aes, self.padding_character).decode('utf-8')))
 
-if(len(sys.argv)>1):
-	client = Client(sys.argv[1])
+if(len(sys.argv)>2):
+	client = Client(sys.argv[1],sys.argv[2])
 else:
-	server = Server()
+	server = Server(sys.argv[1])
 	server.run()
