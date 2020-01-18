e11230d6f635eb50ceab130b9c40647d04f03e88
ToeiKanta <41673034+ToeiKanta@users.noreply.github.com>
2020-01-19 02:35:47 +0700

    Update method to create and join group

--- ChatApp.py
+++ ChatApp.py
@@ -1,6 +1,8 @@
 import socket
 import threading
 import sys, os
+import random
+import string
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'BlockChain'))
 import BlockChain
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'AES_Cyptography'))
@@ -8,6 +10,11 @@ import AES_Cyptography
 
 # Run BlockChain
 blockchain = BlockChain.Blockchain()
+
+def randomString(stringLength=10):
+    """Generate a random string of fixed length """
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(stringLength))
 
 class Server:
 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -49,25 +56,39 @@ class Server:
 
 class Client:
 	key_aes = ''
+	name = ''
+	groupId = ''
 	sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 	def sendMsg(self):
 		while True:
-			data = bytes(input(""),'utf-8')
+			private_msg = input("")
+			secret_key = self.key_aes
+			padding_character = "X"
+			data = bytes("{ groupID: " + str(self.groupId)+ ";name: "+ str(self.name) +";msg: " + str(AES_Cyptography.encrypt_message(private_msg, secret_key, padding_character))  + "}",'utf-8')
 			self.sock.send(data)
 	def __init__(self,address):
 		self.sock.connect((address,20002))
 		print("Welcome to BlockChat")
+		print("Enter your name :")
+		self.name = input("")
 		print("What do you want?")
 		print("enter 1 to create new group:")
 		print("enter 2 to join group:")
 		mode = input("")
+		# create new group
 		if(mode == "1"):
+			# create AES key
 			self.key_aes = AES_Cyptography.generate_secret_key_for_AES_cipher()
-			print("Key generated : " + str(self.key_aes))
+			# create group ID
+			self.groupId = randomString(10)
+			print("GroupID generated : " + str(self.groupId))
+			print("Secrete Group Key generated : " + str(self.key_aes.decode("utf-8")))
 		elif(mode == "2"):
+			print("Enter Group ID :")
+			self.groupId = input("")
 			print("Please enter secrete group key :")
 			self.key_aes = input("")
-			
+
 		iThread = threading.Thread(target=self.sendMsg)
 		iThread.daemon = True
 		iThread.start()
